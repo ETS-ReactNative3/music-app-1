5ff486954b270d0a634a7072ca3b10b5353883a3
Kamalesh <kamalesh.pandey@mycompa.com>
2021-01-24 21:19:16 +0530

    success page card ui

--- app/containers/Wallet/success.js
+++ app/containers/Wallet/success.js
@@ -1,33 +1,86 @@
-import React, {memo, useEffect} from 'react';
-import {createStructuredSelector} from "reselect";
-import {connect} from "react-redux";
-import {compose} from "redux";
-import {useLocation} from "react-router-dom";
-import {createPaymentRequestAction} from "./actions";
-import reducer from "./reducer";
-import saga from "./saga";
-import {useInjectSaga} from 'utils/injectSaga';
-import {useInjectReducer} from 'utils/injectReducer';
+import React, { memo, useEffect } from 'react';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileInvoiceDollar, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import PaperCard from '../../components/PaperCard';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import { createPaymentRequestAction } from './actions';
+import reducer from './reducer';
+import saga from './saga';
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-export function OrderSuccess({createOrder}) {
-  useInjectReducer({key: 'wallet', reducer});
-  useInjectSaga({key: 'wallet', saga});
+export function OrderSuccess({ createOrder }) {
+  useInjectReducer({ key: 'wallet', reducer });
+  useInjectSaga({ key: 'wallet', saga });
 
   const query = useQuery();
 
   useEffect(() => {
-    createOrder(query.get("session_id"));
+    createOrder(query.get('session_id'));
   }, [query]);
 
   return (
-    <div>
-      Success
-    </div>
-  )
+    <>
+    <PaperCard>
+      <Row className="mt-4 justify-content-center">
+        <Col lg={6}>
+          <Card className="bg-transparent blick-border">
+            <ListGroup>
+              <ListGroup.Item className="p-4 bg-transparent border-0 text-center">
+                <FontAwesomeIcon
+                  icon={faFileInvoiceDollar}
+                  className="h1 text-success"
+                />
+                <div className="h1 text-center text-success">Success</div>
+                <div className="m-4">
+                  <small className="h6">Some content here!!</small>
+                </div>
+                <Button role="link" variant="success" type="button">
+                  Go back to wallet
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </PaperCard>
+    <PaperCard>
+    <Row className="mt-4 justify-content-center">
+      <Col lg={6}>
+        <Card className="bg-transparent blick-border__danger">
+          <ListGroup>
+            <ListGroup.Item className="p-4 bg-transparent border-0 text-center">
+              <FontAwesomeIcon
+                icon={faThumbsDown}
+                className="h1 text-danger"
+              />
+              <div className="h1 text-center text-danger">Success</div>
+              <div className="m-4">
+                <small className="h6">Some content here!!</small>
+              </div>
+              <Button role="link" variant="danger" type="button">
+                Go back to wallet
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  </PaperCard>
+  </>
+  );
 }
 
 OrderSuccess.propTypes = {};
@@ -36,7 +89,7 @@ const mapStateToProps = createStructuredSelector({});
 
 function mapDispatchToProps(dispatch) {
   return {
-    createOrder: (id) => dispatch(createPaymentRequestAction(id)),
+    createOrder: id => dispatch(createPaymentRequestAction(id)),
   };
 }
 
@@ -45,4 +98,7 @@ const withConnect = connect(
   mapDispatchToProps,
 );
 
-export default compose(withConnect, memo)(OrderSuccess);
+export default compose(
+  withConnect,
+  memo,
+)(OrderSuccess);
