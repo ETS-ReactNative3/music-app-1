3f874430f00ce0e749759d5f9ff2e40555bea8ed
Kamalesh <kamalesh.pandey@mycompa.com>
2021-01-30 10:42:17 +0530

    trello ui changes for playlist and campaign detail page

--- app/containers/Album/index.js
+++ app/containers/Album/index.js
@@ -4,6 +4,8 @@ import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PaperCard from '../../components/PaperCard';
+import { Col, Image, Row } from 'react-bootstrap';
 import moment from 'moment';
 import {
   makeSelectRecommended,
@@ -78,94 +80,197 @@ const Album = props => {
       {loader || !albumInfo ? (
         <LoadingIndicator />
       ) : (
-        <div className="container-fluid jumbotron-bg-inner">
-          <div className="row album-detail">
-            <div className="col-auto">
-              <div className="profile-img-box">
-                <img
-                  src={albumInfo.artwork}
-                  className="rounded-lg img-fluid"
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="col pt-3 pt-md-0">
-              <div className="row">
-                <div className="col">
-                  <h5>{albumInfo.caption}</h5>
-                  <h1>{albumInfo.title}</h1>
-                </div>
-                <div className="col text-right">
-                  <ShareBox />
-                </div>
-              </div>
-              <div className="row flex-column">
-                <div className="col">
-                  Album | {moment(albumInfo.releaseDate).format('YYYY-MM-DD')} |{' '}
-                  {albumInfo.albumSongs.length}
-                </div>
-                <div className="col mt-3">
-                  <span
-                    onClick={playAllSongsHandler}
-                    className="text-decoration-none bg-white text-dark px-4 py-2 rounded-pill text-center cursor-pointer"
-                  >
-                    {currentSong.playing ? 'Pause' : 'Play All'}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <section className="py-5">
-            {albumInfo.albumSongs.map((ele, index) => (
-              <div
-                className="d-flex border-bottom blick-border border-top-0 border-right-0 border-left-0 align-items-center songs-ul py-3"
-                key={index}
-              >
-                <div className="song-number">{`0${index + 1}`.slice(-2)}</div>
-                <div className="song-title px-2 min-w15">
-                  <h5>{ele.song.title}</h5>
-                  <h6>{ele.song.description}</h6>
-                </div>
-                <div className="song-duration px-2">4:25</div>
-                <div className="song-action px-2">
-                  <span
-                    onClick={() => singleSongHandler(index)}
-                    className="cursor-pointer"
-                  >
-                    <FontAwesomeIcon
-                      size="3x"
-                      color={PLAY_ICON_BG_COLOR}
-                      icon={
-                        currentSong.songIndex === index && currentSong.playing
-                          ? faPauseCircle
-                          : faPlayCircle
-                      }
-                    />
-                  </span>
-                </div>
-                {role && (
-                  <div className="dot-box ml-auto">
-                    <SongsOptionsBox
-                      songId={ele.song.id}
-                      playlists={playlists}
-                      getMyPlaylistAction={getMyPlaylistAction}
-                      createPlaylistandAddSongAction={
-                        createPlaylistandAddSongAction
-                      }
-                      addSongIntoPlaylistAction={addSongIntoPlaylistAction}
-                    />
+        <>
+          <PaperCard title={albumInfo.title}>
+            <Row>
+              <Col md={7} lg={8} xl={9}>
+                <div className="d-flex align-items-center">
+                  <Image
+                    width={150}
+                    height={150}
+                    onError={e => {
+                      e.target.onerror = null;
+                      e.target.src = defaultImage;
+                    }}
+                    src={albumInfo.artwork}
+                    alt=""
+                    roundedCircle
+                  />
+                  <div className="ml-3">
+                    <div className="d-flex align-items-center">
+                      {albumInfo.caption}
+                      <span className="ml-2">
+                        <ShareBox />
+                      </span>
+                    </div>
+                    <small className="text-muted d-block">
+                      {moment(albumInfo.releaseDate).format('YYYY-MM-DD')}
+                    </small>
+                    <small className="text-muted d-block">
+                      Songs: {albumInfo.albumSongs.length}
+                    </small>
+                    <span
+                      onClick={playAllSongsHandler}
+                      className="mt-2 btn btn-warning btn-sm rounded-pill"
+                    >
+                      {currentSong.playing ? 'Pause' : 'Play All'}
+                    </span>
                   </div>
-                )}
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <section className="py-5">
+                  {albumInfo.albumSongs.map((ele, index) => (
+                    <div
+                      className="d-flex border-bottom blick-border border-top-0 border-right-0 border-left-0 align-items-center songs-ul py-2"
+                      key={index}
+                    >
+                      <div className="song-number">
+                        {`0${index + 1}`.slice(-2)}
+                      </div>
+                      <div className="song-title px-2 min-w15">
+                        <h5>{ele.song.title}</h5>
+                        <h6>{ele.song.description}</h6>
+                      </div>
+                      <div className="song-duration px-2">4:25</div>
+                      <div className="song-action px-2">
+                        <span
+                          onClick={() => singleSongHandler(index)}
+                          className="cursor-pointer"
+                        >
+                          <FontAwesomeIcon
+                            size="3x"
+                            color={PLAY_ICON_BG_COLOR}
+                            icon={
+                              currentSong.songIndex === index &&
+                              currentSong.playing
+                                ? faPauseCircle
+                                : faPlayCircle
+                            }
+                          />
+                        </span>
+                      </div>
+                      {role && (
+                        <div className="dot-box ml-auto">
+                          <SongsOptionsBox
+                            songId={ele.song.id}
+                            playlists={playlists}
+                            getMyPlaylistAction={getMyPlaylistAction}
+                            createPlaylistandAddSongAction={
+                              createPlaylistandAddSongAction
+                            }
+                            addSongIntoPlaylistAction={
+                              addSongIntoPlaylistAction
+                            }
+                          />
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </section>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <CarouselFront
+                  list={recommended}
+                  heading="Recommended For You"
+                  clasess="carousel-front py-5"
+                />
+              </Col>
+            </Row>
+          </PaperCard>
+          {/* <div className="container-fluid jumbotron-bg-inner">
+            <div className="row album-detail">
+              <div className="col-auto">
+                <div className="profile-img-box">
+                  <img
+                    src={albumInfo.artwork}
+                    className="rounded-lg img-fluid"
+                    alt=""
+                  />
+                </div>
               </div>
-            ))}
-          </section>
+              <div className="col pt-3 pt-md-0">
+                <div className="row">
+                  <div className="col">
+                    <h5>{albumInfo.caption}</h5>
+                    <h1>{albumInfo.title}</h1>
+                  </div>
+                  <div className="col text-right">
+                    <ShareBox />
+                  </div>
+                </div>
+                <div className="row flex-column">
+                  <div className="col">
+                    Album | {moment(albumInfo.releaseDate).format('YYYY-MM-DD')}{' '}
+                    | {albumInfo.albumSongs.length}
+                  </div>
+                  <div className="col mt-3">
+                    <span
+                      onClick={playAllSongsHandler}
+                      className="text-decoration-none bg-white text-dark px-4 py-2 rounded-pill text-center cursor-pointer"
+                    >
+                      {currentSong.playing ? 'Pause' : 'Play All'}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <section className="py-5">
+              {albumInfo.albumSongs.map((ele, index) => (
+                <div
+                  className="d-flex border-bottom blick-border border-top-0 border-right-0 border-left-0 align-items-center songs-ul py-3"
+                  key={index}
+                >
+                  <div className="song-number">{`0${index + 1}`.slice(-2)}</div>
+                  <div className="song-title px-2 min-w15">
+                    <h5>{ele.song.title}</h5>
+                    <h6>{ele.song.description}</h6>
+                  </div>
+                  <div className="song-duration px-2">4:25</div>
+                  <div className="song-action px-2">
+                    <span
+                      onClick={() => singleSongHandler(index)}
+                      className="cursor-pointer"
+                    >
+                      <FontAwesomeIcon
+                        size="3x"
+                        color={PLAY_ICON_BG_COLOR}
+                        icon={
+                          currentSong.songIndex === index && currentSong.playing
+                            ? faPauseCircle
+                            : faPlayCircle
+                        }
+                      />
+                    </span>
+                  </div>
+                  {role && (
+                    <div className="dot-box ml-auto">
+                      <SongsOptionsBox
+                        songId={ele.song.id}
+                        playlists={playlists}
+                        getMyPlaylistAction={getMyPlaylistAction}
+                        createPlaylistandAddSongAction={
+                          createPlaylistandAddSongAction
+                        }
+                        addSongIntoPlaylistAction={addSongIntoPlaylistAction}
+                      />
+                    </div>
+                  )}
+                </div>
+              ))}
+            </section>
 
-          <CarouselFront
-            list={recommended}
-            heading="Recommended For You"
-            clasess="carousel-front py-5"
-          />
-        </div>
+            <CarouselFront
+              list={recommended}
+              heading="Recommended For You"
+              clasess="carousel-front py-5"
+            />
+          </div> */}
+        </>
       )}
     </>
   );
