b6d81a68e2aa5bffb220784161c230d433ebc6b5
Mayur <mayur.gaud@easternenterprise.com>
2021-02-07 15:39:10 +0530

    Fixed the playlist numbering

--- app/containers/Playlist/detail.js
+++ app/containers/Playlist/detail.js
@@ -123,7 +123,7 @@ function Detail(
                     <div className="song-number pr-3 col-md-1">
                       {`0${index + 1}`.slice(-2)}
                     </div>
-                    <div className="song-title px-2 col-md-4">
+                    <div className="song-title px-2 col-md-5">
                       <h5>{ele.song.title}</h5>
                       <h6>{ele.song.description}</h6>
                     </div>
@@ -155,89 +155,6 @@ function Detail(
           </Row>
         </PaperCard>
       )}
-
-      {/* <div className="container-fluid jumbotron-bg-inner">
-        {playlist && (
-          <>
-            <div className="row album-detail">
-              <div className="col-auto">
-                {/* <div className="profile-img-box"> */}
-      {/*  <img */}
-      {/*    src={albumInfo.artwork} */}
-      {/*    className="rounded-lg img-fluid" */}
-      {/*    alt="" */}
-      {/*  /> */}
-      {/* </div> */}
-
-      {/*
-              </div>
-              <div className="col pt-3 pt-md-0">
-                <div className="row">
-                  <div className="col">
-                    <h1>{playlist.title}</h1>
-                  </div>
-                  <div className="col text-right">{/* <ShareBox /> */}
-      {/*
-                  </div>
-                </div>
-                <div className="row flex-column">
-                  <div className="col">
-                    Playlist |{' '}
-                    {moment(playlist.createdDate).format('YYYY-MM-DD')} |{' '}
-                    {playlist.playlistSongs.length}
-                  </div>
-                  <div className="col mt-3">
-                    {/* <span */}
-      {/*  onClick={playAllSongsHandler} */}
-      {/*  className="text-decoration-none bg-white text-dark px-4 py-2 rounded-pill text-center cursor-pointer" */}
-      {/* > */}
-      {/*  {playing ? 'Pause' : 'Play All'} */}
-      {/* </span> */}
-
-      {/*
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <section className="py-5">
-              {playlist.playlistSongs.map((ele, index) => (
-                <div
-                  className="d-flex border-bottom blick-border border-top-0 border-right-0 border-left-0 align-items-center songs-ul py-3"
-                  key={index}
-                >
-                  <div className="song-number">{`0${index + 1}`.slice(-2)}</div>
-                  <div className="song-title px-2 min-w15">
-                    <h5>{ele.song.title}</h5>
-                    <h6>{ele.song.description}</h6>
-                  </div>
-                  <div className="song-duration px-2">4:25</div>
-                  <div className="song-action px-2">
-                    <span
-                      onClick={() => singleSongHandler(index)}
-                      className="cursor-pointer"
-                    >
-                      <FontAwesomeIcon
-                        size="3x"
-                        color={PLAY_ICON_BG_COLOR}
-                        icon={
-                          songIndex === index && playing
-                            ? faPauseCircle
-                            : faPlayCircle
-                        }
-                      />
-                    </span>
-                  </div>
-                  <div className="dot-box ml-auto">
-                    <PlaylistOptions remove={() => removeSong(ele.song.id)} />
-                  </div>
-                </div>
-              ))}
-            </section>
-          </>
-        )}
-      </div>
-     */}
     </>
   );
 }
