9e7bac1b0c5dd0b47765767f484043410c30e906
Anky's <a.patidar@easternenterprise.com>
2021-02-20 22:58:32 +0530

    Trello issue fix for accept button and link shown

--- app/containers/Campaign/influencerVerify.js
+++ app/containers/Campaign/influencerVerify.js
@@ -129,7 +129,7 @@ const Details = (
             }
           }}
         >
-          Verify this influencer
+          Accept
           {verifySubmitting && <Spinner animation="border" />}
         </Button>
         <Button
