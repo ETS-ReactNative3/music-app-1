4d58a9e89b804200ce8de39a4ffece0f26b1b8a4
Anky's <a.patidar@easternenterprise.com>
2021-02-22 22:04:48 +0530

    Changes on artist page

--- app/containers/Artist/reducer.js
+++ app/containers/Artist/reducer.js
@@ -4,15 +4,30 @@
  *
  */
 import produce from 'immer';
-import { DEFAULT_ACTION } from './constants';
+import { DEFAULT_ACTION, ERROR_ARTIST, FETCH_ARTIST, SAVE_ARTIST } from './constants';
 
-export const initialState = {};
+export const initialState = {
+  artist: {},
+  fetching: false
+};
 
 /* eslint-disable default-case, no-param-reassign */
 const artistReducer = (state = initialState, action) =>
-  produce(state, (/* draft */) => {
+  produce(state, (draft) => {
     switch (action.type) {
       case DEFAULT_ACTION:
+        break;
+      case FETCH_ARTIST:
+        draft.fetching = true;
+        break;
+      case SAVE_ARTIST:
+        draft.artist = action.artist;
+        draft.fetching = false;
+        break
+
+      case ERROR_ARTIST:
+        draft.fetching = false;
+        draft.artist = {};
         break;
     }
   });
