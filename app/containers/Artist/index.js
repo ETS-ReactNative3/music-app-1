0147dfacf83b297aa8aab0220984d88522078806
Mayur <mayur.gaud@easternenterprise.com>
2021-02-27 14:03:33 +0530

    Fixed the api issue

--- app/containers/Artist/index.js
+++ app/containers/Artist/index.js
@@ -4,49 +4,51 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, {memo, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {compose} from 'redux';
 
-import { useInjectSaga } from '../../utils/injectSaga';
-import { useInjectReducer } from '../../utils/injectReducer';
+import {useInjectSaga} from '../../utils/injectSaga';
+import {useInjectReducer} from '../../utils/injectReducer';
 import reducer from './reducer';
 import saga from './saga';
-import Background from '../../images/banner-bg.jpg'
 import './index.scss'
-import { fetchArtistAction, followArtistAction } from './actions';
-import { makeSelectArtist, makeSelectArtistFetching } from './selectors';
+import {fetchArtistAction, followArtistAction} from './actions';
+import {makeSelectArtist, makeSelectArtistFetching} from './selectors';
 import CarouselFront from '../../components/CarouselFront';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 import messages from '../HomePage/messages';
 import LoadingIndicator from '../../components/LoadingIndicator';
-import { useHistory, useParams } from 'react-router-dom';
-import { makeSelectUserDetails } from '../App/selectors';
+import {useHistory, useParams} from 'react-router-dom';
+import {makeSelectUserDetails} from '../App/selectors';
 
-export function Artist({ artist, fetchArtist, artistFetching, followArtist, userDetails }) {
-  useInjectReducer({ key: 'artist', reducer });
-  useInjectSaga({ key: 'artist', saga });
+export function Artist({artist, fetchArtist, artistFetching, followArtist, userDetails}) {
+  useInjectReducer({key: 'artist', reducer});
+  useInjectSaga({key: 'artist', saga});
   const {id} = useParams();
 
   const history = useHistory();
-  React.useEffect(() => {
+  useEffect(() => {
     fetchArtist(id)
   }, [id])
+
   return (
     <div>
-      {!(artistFetching || Object.keys(artist).length === 0) ? <><section className="banner" style={{ backgroundImage: `url(${artist.coverPhoto})` }}>
-        <div className="container h-100">
-          <div className="row h-100 justify-content-center align-items-center">
-            <div className="col-12">
-              <h1 className="display-4">{artist.name}</h1>
-              <hr className="my-4" />
-              <a onClick={() =>  (userDetails) ? followArtist(artist): history.push('/login')} className="btn btn-primary btn-lg" href="#" role="button">Follow</a>
+      {!(artistFetching || Object.keys(artist).length === 0) ? <>
+        <section className="banner" style={{backgroundImage: `url(${artist.coverPhoto})`}}>
+          <div className="container h-100">
+            <div className="row h-100 justify-content-center align-items-center">
+              <div className="col-12">
+                <h1 className="display-4">{artist.name}</h1>
+                <hr className="my-4"/>
+                <button onClick={() => (userDetails) ? followArtist(artist) : history.push('/auth/login')}
+                   className="btn btn-primary btn-lg">Follow</button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
         <section className="artist-data">
           <CarouselFront
             loading={artistFetching}
@@ -54,7 +56,8 @@ export function Artist({ artist, fetchArtist, artistFetching, followArtist, user
             heading={<FormattedMessage {...messages.featuredAlbumHeading} />}
             clasess="carousel-front py-5"
           />
-        </section></>: <LoadingIndicator/>}
+        </section>
+      </> : <LoadingIndicator/>}
     </div>
   );
 }
