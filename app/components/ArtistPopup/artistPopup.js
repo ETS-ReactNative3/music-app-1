6bd4872aa5744914beedd83b917246ae94f1c744
Mayur <mayur.gaud@easternenterprise.com>
2021-03-24 22:56:49 +0530

    Corrected the bio popup

--- app/components/ArtistPopup/artistPopup.js
+++ app/components/ArtistPopup/artistPopup.js
@@ -1,46 +1,24 @@
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Col, Container, Image, Modal, Row } from 'react-bootstrap';
-import defaultImage from '../../images/album-1.jpg';
+import {Modal} from 'react-bootstrap';
 
-const ArtistPopup = ({ artist, handleClose, showMoreInfo}) => {
-    
-    return (
-        <Modal
-            show={showMoreInfo}
-            onHide={handleClose}
-            backdrop="static"
-            keyboard={false}
-            size="lg"
-        >
-
-            <Container>
-
-                <FontAwesomeIcon icon={faTimes} style={{ position: 'absolute', left: 0, top: 0, backgroundColor: 'red', cursor: 'pointer' }} onClick={() => { handleClose() }} />
-                <div className="">
-                    <Image
-                        width={'100%'}
-                        height={250}
-                        onError={e => {
-                            e.target.onerror = null;
-                            e.target.src = defaultImage;
-                        }}
-                        src={artist.coverPhoto || ''}
-                        alt="user avatar"
-                    />
-                </div>
-                <Row>
-                    <Col>
-                        <h2>
-                            {artist.name}
-                        </h2>
-                    </Col>
-                </Row>
-               
-            </Container>
-        </Modal>
-    )
+const ArtistPopup = ({artist, handleClose, showMoreInfo}) => {
+  return (
+    <Modal
+      show={showMoreInfo}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      size="lg"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>{artist.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h3>Biography</h3>
+        <p>{artist.artistInformation.biography}</p>
+      </Modal.Body>
+    </Modal>
+  )
 }
 export default ArtistPopup;
 
