a582351d7c06ddd0fa37b821e433b95cfbb5e014
Mayur <mayur.gaud@easternenterprise.com>
2021-05-16 22:00:53 +0530

    Fixed the subscription plans

--- app/containers/Subscription/index.js
+++ app/containers/Subscription/index.js
@@ -15,6 +15,8 @@ import {toast} from "react-toastify";
 import {loadStripe} from "@stripe/stripe-js";
 import PaperCard from "../../components/PaperCard";
 import './index.scss';
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const stripePromise = loadStripe('pk_test_KcTV8d4CSSGpMfe4PIKvUeFI00hDyI8a1d');
 
@@ -60,41 +62,48 @@ export function Subscription({getPlansList, subscriptionPlans, subscriptionLoade
               <div className="card-header border-light py-5 px-4">
                 <div className="d-flex mb-3">
                   <span className="h5 mb-0">$</span>
-                  <span className="price display-2 mb-0">{item.price}</span>
+                  <span className="price display-2 mb-0">{item.price / 100}</span>
                   {/*<span className="h6 font-weight-normal align-self-end">/month</span>*/}
                 </div>
                 <h4 className="mb-3 text-black">{item.title}</h4>
-                <p className="font-weight-normal mb-0">If you're new to SEO or just need the basics, this plan is a
-                  great way.</p>
               </div>
               <div className="card-body pt-5">
                 <ul className="list-group simple-list">
                   <li className="list-group-item font-weight-normal">
-                    <span className="icon-gray"><i className="fas fa-check"/></span>
-                    Users<span className="font-weight-bolder"> 1</span>
+                    <span className="icon-gray">
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Ad-free listening
+                    </span>
                   </li>
                   <li className="list-group-item font-weight-normal">
                     <span className="icon-gray">
-                      <i className="fas fa-check"/>
-                    </span>Full access to design tool
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Access full catalog
+                    </span>
                   </li>
                   <li className="list-group-item font-weight-normal">
                     <span className="icon-gray">
-                      <i className="fas fa-check"/></span>Publish with
-                    <span className="font-weight-bolder">Rocket</span> ® label
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Expertly curated playlists
+                    </span>
                   </li>
                   <li className="list-group-item font-weight-normal">
-                    <span className="icon-gray"><i className="fas fa-check"/>
-                    </span>Sub-domain publish
+                    <span className="icon-gray">
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Automatic entry into draws & competitions
+                    </span>
                   </li>
                   <li className="list-group-item font-weight-normal">
-                    <span className="icon-gray"><i className="fas fa-check"/></span>1 User
+                    <span className="icon-gray">
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Earn Rewards
+                    </span>
                   </li>
-                  <li className="list-group-item font-weight-normal"><span className="icon-gray"><i
-                    className="fas fa-check"/></span>Public Share &amp; Comments
-                  </li>
-                  <li className="list-group-item font-weight-normal"><span className="icon-gray"><i
-                    className="fas fa-check"/></span>Chat Support
+                  <li className="list-group-item font-weight-normal">
+                    <span className="icon-gray">
+                      <FontAwesomeIcon icon={faCheck} className="mr-2"/>
+                      Support your chosen artists directly
+                    </span>
                   </li>
                 </ul>
               </div>
