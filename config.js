9d5943bc7e1234745ab4d3edb3f025570fbbd600
Mayur <mayur.gaud@easternenterprise.com>
2021-11-01 12:38:27 +0530

    Staging config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = module.exports = {};
 
 config.server = {
-  apiURL: 'https://bliiink-svfjg.ondigitalocean.app/',
+  apiURL: 'https://bliiinkstaging-3t9ns.ondigitalocean.app/',
   trackingApiURL: 'https://track.bliiink.ga/',
 };
