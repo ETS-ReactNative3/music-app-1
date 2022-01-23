1210c86f58c4de531f54ddc6f5f1a6e293f4445c
Mayur <mayur.gaud@easternenterprise.com>
2022-01-23 19:54:05 +0530

    changed the url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ let config = module.exports = {};
 
 config.server = {
   apiURL: 'https://bliiinkstaging-3t9ns.ondigitalocean.app/',
-  trackingApiURL: 'https://track.bliiink.ga/',
+  trackingApiURL: 'https://track.bliiink.net/',
 };
